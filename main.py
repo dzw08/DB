6166b39cf79169daafa03ad686710364610e7605
dzw08 <dwloucs@outlook.com>
2026-02-23 17:11:51 +0000

    Reading eso

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 """Reads DesignBuilder EnergyPlus Output files."""
 
 # libraries
-from db_eplusout_reader import Variable, DBEsoFile
-from db_eplusout_reader.constants import RP
+from db_eplusout_reader import Variable, DBEsoFile, get_results
+from db_eplusout_reader.constants import RP, H
 import tkinter as tk
 import matplotlib as mpl
+
+# parse eso file, usually ask for user input, however using example for now
+
+#print("Enter eso file path")
+#path = input()
+path = r"/home/dani/DB/eplusout.eso"
+eso = DBEsoFile.from_path(path)  
