51bfefc24a35c0d695396e37b51f63f7c1dd7bf3
dzw08 <dwloucs@outlook.com>
2026-02-24 19:28:58 +0000

    Functioning with barebones functionality

--- kivy_implementation.py
+++ kivy_implementation.py
@@ -10,13 +10,17 @@ import matplotlib.pyplot as plt
 from db_eplusout_reader import Variable, get_results
 from db_eplusout_reader.constants import H
 from kivy.app import App
+from kivy.graphics import Color, Rectangle
 from kivy.resources import resource_add_path
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy_garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from numpy import arange
 
-# parse output file, usually ask for user input, however using example for now
-
+# usually ask for user input, however using example for now
 # print("Enter eso file path")
 # path = input()
 PATH_TO_FILE = r"/home/dani/DB/eplusout.sql"
@@ -79,7 +83,7 @@ def plot_results(results_values, results_keys, data_type):
     for i in range(hours_passed):
         hours.append(i)
 
-    plt.figure(dpi=100, layout="constrained")
+    plt.figure(dpi=100)
     # enumerate through the headers to label each line
     for i, keys in enumerate(results_keys):
         plt.plot(
@@ -96,23 +100,70 @@ def plot_results(results_values, results_keys, data_type):
     return plt
 
 
-# example usage
-temperature_results, temperature_result_keys, temperature_result_values = (
-    collect_temperature_results(PATH_TO_FILE)
-)
-plot = plot_results(temperature_result_values, temperature_result_keys, "Temperature")
-
-
 # setting up front page class
-class FrontPage(BoxLayout):
-    def show(self):
-        self.add_widget(FigureCanvasKivyAgg(plot.gcf()))
+class HomeScreen(Screen):
+    pass
+
+
+# setting up screen that shows the plotted data
+class DataScreen(Screen):
+    def __init__(self, **kwargs):
+        super(DataScreen, self).__init__(**kwargs)
+        layout = FloatLayout()
+        layout1 = BoxLayout(padding=5, orientation="horizontal", size_hint=(1, 0.3))
+
+        # adding background
+        with layout1.canvas.before:
+            Color(rgba=(0.129, 0.149, 0.192, 1))
+            self._header_rect = Rectangle(size=layout1.size, pos=layout1.pos)
+        layout1.bind(
+            size=lambda inst, val: setattr(self._header_rect, "size", val),
+            pos=lambda inst, val: setattr(self._header_rect, "pos", val),
+        )
+
+        # assigning the plotted Kivy-compatible matplotlib graph to variable
+        plot = self.plot_graph()
+        graph = FigureCanvasKivyAgg(plot.gcf())
+        # ensure the graph fills the space above the bottom bar
+        graph.size_hint = (1, 0.7)
+        graph.pos_hint = {"x": 0, "y": 0.3}
+
+        layout.add_widget(graph)
+        layout1.add_widget(Label(text="Graph"))
+        layout1.add_widget(
+            Button(
+                text="Back",
+                on_press=lambda inst: (
+                    setattr(self.manager.transition, "direction", "right")
+                    or setattr(self.manager, "current", "home")
+                ),
+            )
+        )
+
+        layout.add_widget(layout1)
+
+        self.add_widget(layout)
+
+    def plot_graph(self):
+        temperature_results, temperature_result_keys, temperature_result_values = (
+            collect_temperature_results(PATH_TO_FILE)
+        )
+        plot = plot_results(
+            temperature_result_values, temperature_result_keys, "Temperature"
+        )
+        return plot
 
 
 # setting up app class from KivyApp for main app
 class ResultsPlotterApp(App):
     def build(self):
-        return FrontPage()
+        sm = ScreenManager()
+        # default to sliding right for any screen change
+        sm.transition.direction = "right"
+        sm.add_widget(HomeScreen(name="home"))
+        sm.add_widget(DataScreen(name="data"))
+
+        return sm
 
 
 # run the app
