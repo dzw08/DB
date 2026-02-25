9bcf1501b3c2afa2428a9d41fc75e1bf52808b50
dzw08 <dwloucs@outlook.com>
2026-02-25 15:00:45 +0000

    Updated no_gui version, treating it as the backend, removing functions from GUI version.

--- main.py
+++ main.py
@@ -4,9 +4,6 @@
 import os
 import sys
 
-import matplotlib.pyplot as plt
-from db_eplusout_reader import Variable, get_results
-from db_eplusout_reader.constants import RP, TS, A, D, H, M
 from kivy.app import App
 from kivy.graphics import Color, Rectangle
 from kivy.resources import resource_add_path
@@ -17,98 +14,8 @@ from kivy.uix.label import Label
 from kivy.uix.modalview import ModalView
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy_garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
-from numpy import arange
 
-
-def collect_temperature_results(path: str, freq: str):
-    """Using db_eplusout_reader to extract temperature results from eplusout SQL file.
-
-    Parameters
-    ----------
-    path
-        location of sql file to be parsed
-    freq
-        frequency of the data collection
-
-    Returns
-    -------
-    temp_results
-        dictionary
-        Raw ResultsDictionary object of file parsing.
-    temp_results_keys
-        list
-        Location, description and units of temperatures.
-    temp_results_values
-        list
-        Raw values of temperatures.
-    """
-
-    frequencies = {"TS": TS, "H": H, "D": D, "M": M, "A": A, "RP": RP}
-    temp_results = get_results(
-        path, variables=[Variable(None, None, "C")], frequency=frequencies[freq]
-    )
-
-    # retrieving actual temp values as well as locations
-    temp_results_values = list(temp_results.values())
-    temp_results_keys = list(temp_results.keys())
-
-    return freq, temp_results_keys, temp_results_values
-
-
-def plot_results(results_values: list, results_keys: list, data_type: str, freq: str):
-    """Plots db-eplusout-reader output files in a line graph using matplotlib.
-
-    Parameters
-    ----------
-    results_values
-        LIST The actual values of the data.
-    results_keys
-        LIST What the values of the data refer to as well as their units.
-    data_type
-        STR What kind of data, e.g. Temperature or energy
-
-    Returns
-    -------
-    Plotted matplotlib data
-    """
-
-    units_passed = len(results_values[0])
-    time_units = list(range(units_passed))
-
-    plt.figure(dpi=100)
-    for i, keys in enumerate(results_keys):
-        plt.plot(
-            time_units,
-            results_values[i],
-            label=f"{keys[0]} {keys[1]}",
-        )
-
-    # labels
-    if freq == "TS":
-        step = 1
-        unit = "Time steps"
-    if freq == "H":
-        step = 24
-        unit = "Days"
-    if freq == "D":
-        step = 1
-        unit = "Days"
-    if freq == "M":
-        step = 1440
-        unit = "Months"
-    if freq == "A":
-        step = 525600
-        unit = "Years"
-    if freq == "RP":
-        step = 1
-        unit = "Run Period"
-    plt.xticks(
-        arange(0, units_passed + 1, step), (arange(0, units_passed + 1, step) / step)
-    )
-    plt.xlabel(f"{unit} passed")
-    plt.ylabel(f"{data_type} in {results_keys[0][2]}")
-    plt.title(f"{data_type} results")
-    return plt
+from no_gui import collect_temperature_results, plot_results
 
 
 class HomeScreen(Screen):
--- no_gui.py
+++ no_gui.py
@@ -3,29 +3,28 @@
 # libraries
 import matplotlib.pyplot as plt
 from db_eplusout_reader import Variable, get_results
-from db_eplusout_reader.constants import H
+from db_eplusout_reader.constants import RP, TS, A, D, H, M
 from numpy import arange
 
 # parse output file, usually ask for user input, however using example for now
-
-# print("Enter eso file path")
-# path = input()
 PATH_TO_FILE = r"./sample_sql_files/eplusout_hourly.sql"
 
 
-def collect_temperature_results(path):
+def collect_temperature_results(path: str, freq: str):
     """Using db_eplusout_reader to extract temperature results from eplusout SQL file.
 
     Parameters
     ----------
     path
         location of sql file to be parsed
+    freq
+        frequency of the data collection
 
     Returns
     -------
     temp_results
         dictionary
-        Raw Results object of file parsing.
+        Raw ResultsDictionary object of file parsing.
     temp_results_keys
         list
         Location, description and units of temperatures.
@@ -33,30 +32,28 @@ def collect_temperature_results(path):
         list
         Raw values of temperatures.
     """
-    temp_results = get_results(path, variables=[Variable(None, None, "C")], frequency=H)
 
-    # casting to list so can be serialized to json object, just using values
+    frequencies = {"TS": TS, "H": H, "D": D, "M": M, "A": A, "RP": RP}
+    temp_results = get_results(
+        path, variables=[Variable(None, None, "C")], frequency=frequencies[freq]
+    )
+
+    # retrieving actual temp values as well as locations
     temp_results_values = list(temp_results.values())
-    # with open("temp_results_values.json", "w") as f:
-    #    dump(temp_results_values, f, indent=4)
-
-    # casting to list so can be serialied to json objects, just using headers/keys
     temp_results_keys = list(temp_results.keys())
-    # with open("temp_results_keys.json", "w") as f:
-    #    dump(temp_results_keys, f, indent=4)
 
-    return temp_results, temp_results_keys, temp_results_values
+    return freq, temp_results_keys, temp_results_values
 
 
-def plot_results(results_values, results_keys, data_type):
+def plot_results(results_values: list, results_keys: list, data_type: str, freq: str):
     """Plots db-eplusout-reader output files in a line graph using matplotlib.
 
     Parameters
     ----------
     results_values
-        ARRAY The actual values of the data.
+        LIST The actual values of the data.
     results_keys
-        ARRAY What the values of the data refer to as well as their units.
+        LIST What the values of the data refer to as well as their units.
     data_type
         STR What kind of data, e.g. Temperature or energy
 
@@ -64,34 +61,44 @@ def plot_results(results_values, results_keys, data_type):
     -------
     Plotted matplotlib data
     """
-    # plot the results using matplotlib
-    hours_passed = len(results_values[0])
-    hours = []
-    for i in range(hours_passed):
-        hours.append(i)
 
-    plt.figure(figsize=(10, 6), dpi=100, layout="constrained")
-    # enumerate through the headers to label each line
+    units_passed = len(results_values[0])
+    time_units = list(range(units_passed))
+
+    plt.figure(dpi=100)
     for i, keys in enumerate(results_keys):
         plt.plot(
-            hours,
+            time_units,
             results_values[i],
             label=f"{keys[0]} {keys[1]}",
-        )  # plotting the data on the axes
+        )
 
     # labels
-    plt.xticks(arange(0, hours_passed + 1, 24))
-    plt.xlabel("Hours passed")
+    if freq == "TS":
+        step = 1
+        unit = "Time steps"
+    if freq == "H":
+        step = 24
+        unit = "Days"
+    if freq == "D":
+        step = 1
+        unit = "Days"
+    if freq == "M":
+        step = 1440
+        unit = "Months"
+    if freq == "A":
+        step = 525600
+        unit = "Years"
+    if freq == "RP":
+        step = 1
+        unit = "Run Period"
+    plt.xticks(
+        arange(0, units_passed + 1, step), (arange(0, units_passed + 1, step) / step)
+    )
+    plt.xlabel(f"{unit} passed")
     plt.ylabel(f"{data_type} in {results_keys[0][2]}")
     plt.title(f"{data_type} results")
-    plt.legend()
-    plt.show()
+    return plt
 
-
-# example usage
-temperature_results, temperature_result_keys, temperature_result_values = (
-    collect_temperature_results(PATH_TO_FILE)
-)
-plot_results(temperature_result_values, temperature_result_keys, "Temperature")
 
 # EOF (End-Of-File)
